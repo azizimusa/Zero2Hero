d2957414722225cd9a0a33f8df3839c1567b2ecd
azizimusa@azizi2u.com <Aswaja77>
2020-09-15 18:50:47 +0800

    working on postcard design

--- app/src/main/java/zero/to/hero/MainActivity.java
+++ app/src/main/java/zero/to/hero/MainActivity.java
@@ -1,8 +1,14 @@
 package zero.to.hero;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 
@@ -24,6 +30,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.custom_toolbar);
+        TextView tt = getSupportActionBar().getCustomView().findViewById(R.id.title);
+        tt.setText("MyPostcards");
+
         Call<PostcardPojo> call = Zero2HeroApp.getInstance().getRestApi().getPostcards(Utils.getToken());
         call.enqueue(new Callback<PostcardPojo>() {
             @Override
@@ -42,4 +53,23 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.setting) {
+            Timber.e("setting opened");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
 }
--- app/src/main/java/zero/to/hero/restApi/RestApi.java
+++ app/src/main/java/zero/to/hero/restApi/RestApi.java
@@ -1,5 +1,8 @@
 package zero.to.hero.restApi;
 
+import android.media.Image;
+
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
@@ -44,4 +47,31 @@ public interface RestApi {
         }
 
     }
+
+//    id	Int64	PostCard ID
+//    card_id	Int64	Card ID
+//    user_id	Int64	User ID
+//    timestamp	Int64	Date in miliseconds
+//    type	String	Card Type. <"WP"|"BP">
+//    name	String	Name in the brandcard
+//    icon	String	Card icon url
+//    description	String	Designation, caption etc in the brandcard
+//    color	String	RGB in HEX
+//    email	String	Optional
+//    background_type	Int	1 = Image, 2 = Video
+//    background_url	String
+//    background_width	Int	"Since not all media able to get the width, server might returned this as NULL.
+//    I suggest client side detects the width or just assume vertical :D"
+//    background_height	Int	"Since not all media able to get the height, server might returned this as NULL.
+//    I suggest client side detects the width or just assume vertical :D"
+//    co_name	String	Company name    (shown if type is BP)
+//    co_icon	String	Company icon url  (shown if type is BP)
+//    liked	Boolean
+//    like_count	Int
+//    viewed	Boolean
+//    view_count	Int
+//    shared	Boolean
+//    share_count	Int
+//    comments	[COMMENT]
+//    body	String	Post body
 }
